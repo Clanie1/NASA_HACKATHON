03ae474c9b940e3675bd9bae9c8f4309633115e5
clanie1 <danybarociov@gmail.com>
2024-10-05 16:19:37 -0700

    supabase client

--- API/src/index.ts
+++ API/src/index.ts
@@ -1,6 +1,7 @@
 // src/index.js
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import supabaseClient from "./supabaseClient";
 
 dotenv.config();
 
@@ -9,6 +10,14 @@ const port = process.env.PORT || 3000;
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
+});
+
+app.get("/users", async (req: Request, res: Response) => {
+  const { data, error } = await supabaseClient.from("client").select();
+  if (error) {
+    res.status(400).json({ error: error.message });
+  }
+  res.status(200).json(data);
 });
 
 app.listen(port, () => {

d38eb471f07a17b42321f2ad1e977dac5e43fa87
Raul Valdes <civza696@outlook.com>
2024-10-05 16:05:26 -0700

    finished sendEmail endpoint

--- API/src/index.ts
+++ API/src/index.ts
@@ -1,14 +1,30 @@
 // src/index.js
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import { sendEmail } from "./utils";
 
 dotenv.config();
 
 const app: Express = express();
+app.use(express.json());
+
 const port = process.env.PORT || 3000;
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
+});
+
+app.post("/api/email", async (req: Request, res: Response) => {
+  const { to, subject } = req.body;
+
+  try {
+    sendEmail(to, subject);
+    console.log('success');
+  } catch (e) {
+    console.log('error');
+  }
+
+  res.send("oki");
 });
 
 app.listen(port, () => {

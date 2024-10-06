8c313b38759b344c9413be0da48705e8f484c4dd
ericpapazzz <ericrendong@gmail.com>
2024-10-05 19:44:27 -0700

    a

--- SPA/spa/src/App.js
+++ SPA/spa/src/App.js
@@ -1,40 +1,9 @@
-import Navbar from "./components/common/Navbar";
-import LandingPage from "./components/LandingPage";
-
-import { useRef } from "react";
-
+import logo from './logo.svg';
+import './App.css';
+import Component from './component';
 
 function App() {
-  //Scroll function
-  const section1Ref = useRef(null);
-  const section2Ref = useRef(null);
-  const section3Ref = useRef(null);
-
-  const scrollToSection = (section) => {
-    let topPosition = 0;
-
-    if (section === "home") {
-      topPosition =
-        section3Ref.current.getBoundingClientRect().top + window.scrollY - 100;
-    } else if (section === "subs") {
-      topPosition =
-        section2Ref.current.getBoundingClientRect().top + window.scrollY - 100;
-    } else if (section === "services") {
-      topPosition =
-        section1Ref.current.getBoundingClientRect().top + window.scrollY - 100;
-    } else if (section === "contact") {
-      topPosition =
-        section2Ref.current.getBoundingClientRect().top + window.scrollY - 100;
-    }
-
-    window.scrollTo({
-      top: topPosition,
-      behavior: "smooth",
-    });
-  };
-
   return (
-
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -51,16 +20,6 @@ function App() {
         </a>
         <Component />
       </header>
-
-    <div className="App w-full relative font-hnLight">
-      <Navbar scrollToSection={scrollToSection} />
-      <LandingPage
-        info={section1Ref}
-        subscribe={section2Ref}
-        home={section3Ref}
-        scrollToSection={scrollToSection}
-      />
-
     </div>
   );
 }

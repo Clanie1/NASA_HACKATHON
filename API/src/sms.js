68faf07cca78f5f84d5977084b62eba2889c1ade
AdrianFernandez32 <oscar.adrian.fersa@gmail.com>
2024-10-05 19:44:02 -0700

    idk wtf i did

--- API/src/sms.js
+++ API/src/sms.js
@@ -1,17 +1,18 @@
-// Download the helper library from https://www.twilio.com/docs/node/install
-const twilio = require("twilio"); // Or, for ESM: import twilio from "twilio";
+const dotenv = require("dotenv");
+const twilio = require("twilio");
+
+dotenv.config();
 
 // Find your Account SID and Auth Token at twilio.com/console
-// and set the environment variables. See http://twil.io/secure
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = twilio(accountSid, authToken);
 
 async function createMessage() {
   const message = await client.messages.create({
-    body: "This is the ship that made the Kessel Run in fourteen parsecs?",
-    from: "+15017122661",
-    to: "+15558675310",
+    body: "Quiero uwu",
+    from: "+15204453256",
+    to: "+526461453036",
   });
 
   console.log(message.body);

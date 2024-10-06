5a81f14fbad7de5274174cee62be1aafc3012e51
clanie1 <danybarociov@gmail.com>
2024-10-06 08:54:33 -0700

    deleted endpoints

--- API/src/index.ts
+++ API/src/index.ts
@@ -21,27 +21,6 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
 });
 
-app.post("/api/email", async (req: Request, res: Response) => {
-  const { to, subject } = req.body;
-
-  try {
-    sendWelcomeEmail(to, subject);
-    console.log("success");
-  } catch (e) {
-    console.log("error");
-  }
-
-  res.send("oki");
-});
-
-app.get("/users", async (req: Request, res: Response) => {
-  const { data, error } = await supabaseClient.from("client").select();
-  if (error) {
-    res.status(400).json({ error: error.message });
-  }
-  res.send(data);
-});
-
 app.post("/createUser", async (req: Request, res: Response) => {
   const { error } = await supabaseClient.from("client").insert(req.body);
   if (error) {
@@ -56,16 +35,6 @@ app.post("/api/gpt", async (req: Request, res: Response) => {
   const { prompt } = req.body;
   const response = sendGptPrompt(prompt);
   res.status(200).json(response);
-});
-
-app.get("/wheaterData", async (req: Request, res: Response) => {
-  const wheaterData = await getWheaterApiData();
-  const disastersData = await getDisasterApiData();
-  const response = {
-    wheater: wheaterData,
-    disastersData: disastersData,
-  };
-  res.send(response);
 });
 
 app.listen(port, () => {

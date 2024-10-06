16d0564e3b9bb7c9697ecb2adf4bbf28ae2bc3de
clanie1 <danybarociov@gmail.com>
2024-10-05 18:19:57 -0700

    no se

--- API/src/index.ts
+++ API/src/index.ts
@@ -36,6 +36,14 @@ app.get("/users", async (req: Request, res: Response) => {
   res.status(200).json(data);
 });
 
+app.post("/createUser", async (req: Request, res: Response) => {
+  const { data, error } = await supabaseClient.from("client").insert(req.body);
+  if (error) {
+    res.status(400).json({ error: error.message });
+  }
+  res.status(200).json(data);
+});
+
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
 });

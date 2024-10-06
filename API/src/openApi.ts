d9c5b1073f8009681fbfa080604e8035f4f367e8
AdrianFernandez32 <oscar.adrian.fersa@gmail.com>
2024-10-06 10:46:56 -0700

    por que tengo un commit?

--- API/src/openApi.ts
+++ API/src/openApi.ts
@@ -4,28 +4,28 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const openai = new OpenAI({apiKey: `${process.env.OPENAI_API_KEY}`});
+const openai = new OpenAI({ apiKey: `${process.env.OPENAI_API_KEY}` });
 
-const sendGptPrompt = async(prompt: string) => {
-    try {
-        const response = await fetch("https://api.openai.com/v1/chat/completions", {
-            method: "POST",
-            headers: {
-                "Content_Type": "application/json",
-                "Authorization": `Bearer ${process.env.OPENAI_API_KEY}`
-            },
-            body: JSON.stringify({
-                model: "gpt-40-mini",
-                messages: [{role: "user", content: prompt}],
-            })
-        });
+const sendGptPrompt = async (prompt: string) => {
+  try {
+    const response = await fetch("https://api.openai.com/v1/chat/completions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+      },
+      body: JSON.stringify({
+        model: "gpt-4",
+        messages: [{ role: "user", content: prompt }],
+      }),
+    });
 
-        const data = await response.json();
-        console.log(data);
-        return data;
-    } catch (e) {
-        console.log("Error: ", e);
-    }
-}
+    const data = await response.json();
+    console.log(data);
+    return data;
+  } catch (e) {
+    console.log("Error: ", e);
+  }
+};
 
-export {sendGptPrompt}
+export { sendGptPrompt };
